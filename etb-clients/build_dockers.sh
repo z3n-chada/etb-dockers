34ae6aadffcd61b88253f49f38835b91afed1193
parithosh <parithosh.jayanthi@ethereum.org>
2022-08-29 12:10:08 +0200

    adding no-cache build

--- etb-clients/build_dockers.sh
+++ etb-clients/build_dockers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-BUILDKIT=1 docker build -t etb-all-clients -f etb-all-clients.Dockerfile .
-BUILDKIT=1 docker build -t etb-all-clients:inst -f etb-all-clients_inst.Dockerfile .
+BUILDKIT=1 docker build --no-cache -t etb-all-clients -f etb-all-clients.Dockerfile .
+BUILDKIT=1 docker build --no-cache -t etb-all-clients:inst -f etb-all-clients_inst.Dockerfile .
--- execution-clients/build_dockers.sh
+++ execution-clients/build_dockers.sh
@@ -4,7 +4,7 @@ for df in $(ls | grep Dockerfile); do
     echo $df
     i=`echo $df | tr '_' ':'`
     image=`echo "${i::-11}"`
-    BUILDKIT=1 docker build -f "$df" -t "$image" .
+    BUILDKIT=1 docker build --no-cache -f "$df" -t "$image" .
 done
 
 
