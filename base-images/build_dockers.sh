447b162587c101ce8fd695ce260646ca50614790
z3n <tyler@ethereum.org>
2022-09-06 10:07:12 -0500

    nethermind + no-cache

--- base-images/build_dockers.sh
+++ base-images/build_dockers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-BUILDKIT=1 docker build -t etb-client-builder -f etb-client-builder.Dockerfile .
-BUILDKIT=1 docker build -t etb-client-runner -f etb-client-runner.Dockerfile .
+BUILDKIT=1 docker build --no-cache -t etb-client-builder -f etb-client-builder.Dockerfile .
+BUILDKIT=1 docker build --no-cache -t etb-client-runner -f etb-client-runner.Dockerfile .
--- fuzzers/build_dockers.sh
+++ fuzzers/build_dockers.sh
@@ -2,5 +2,5 @@
 
 ./build-tx-fuzzer.sh
 
-BUILDKIT=1 docker build -t geth:bad-block-creator -f geth_bad-block-creator.Dockerfile .
-BUILDKIT=1 docker build -t geth:bad-block-creator-inst -f geth_bad-block-creator-inst.Dockerfile .
+BUILDKIT=1 docker build --no-cache -t geth:bad-block-creator -f geth_bad-block-creator.Dockerfile .
+BUILDKIT=1 docker build --no-cache -t geth:bad-block-creator-inst -f geth_bad-block-creator-inst.Dockerfile .
